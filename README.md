b5654112ae5044ba7da6f0ceff47af957f99ed40
matteotolloso <matteotolloso@gmail.com>
2023-06-29 13:50:44 +0200

    new version

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ To install pytorch on linux, run:
 
     conda create -n dnabert python=3.9 -y && conda activate dnabert
 
-    pip3 install torch torchvision torchaudio transformers
+    pip3 install torch torchvision torchaudio transformers biopython
 
     wget 'https://unipiit-my.sharepoint.com/:u:/g/personal/m_tolloso_studenti_unipi_it/EZLI20s-jLhLvx9SGRLmpEABYzh5QBkV4cSl-wfbVgfu2w?e=kT998J&download=1' -O pytorch_model.bin
     mv pytorch_model.bin dnabert/pytorch_model.bin
--- my_embed.py
+++ my_embed.py
@@ -1,25 +1,29 @@
 from typing import Tuple
 from pathlib import Path
-import json
 import numpy as np
+import torch
+from transformers import BertModel, BertConfig, AutoTokenizer, BertTokenizerFast
+import sys
+import os
+import time
+from Bio.Seq import Seq
+from Bio import SeqIO
+from multiprocessing import Process
+
 
 # ********* SETTINGS **********
 
+
+
+FASTA_FILE_PATH = "../BioEmbedding/dataset/topo/topo.fasta"
+OUT_DIR = "../BioEmbedding/dataset/topo/embeddings/dnabert"
+MAX_CHUNK_SIZE = 512
+FAST_MODE = False
+
+
 DNABERT_PATH = Path("dnabert")
 
-FILE_PATH = "../BioEmbedding/dataset/globins/globins.json" # file containing the origina dataset. A key will be added on the dict and the file will be overwrited
-
-
-ANNOTATION_KEY = "dnabert"   # the key to add
-
-# *****************************
-
-
-import torch
-from transformers import BertModel, BertConfig, AutoTokenizer, BertTokenizerFast
-
-
-
+# ******************************
 
 
 def split_sequence(seq: str, k: int=6) -> str:
@@ -56,56 +60,133 @@ def load_dnabert() -> Tuple[BertModel, BertTokenizerFast]:
     return model, tokenizer
 
 
-def compute_embeddings(device: torch.device) -> None:
-    
+def predict(id, chunk_index, query_sequence, write_to_path=False):
 
-    with open(FILE_PATH, "r") as file:
-        seq_dict = json.load(file)
-        
+    start = time.time()
+
+    device = "cpu"
+    device = torch.device(device)
     model, tokenizer = load_dnabert()
     model = model.to(device)
+
+    print(len(query_sequence), file=sys.stderr, flush=True)
+
+
+    kmerized_sequence = split_sequence(query_sequence)
     
-    for k in seq_dict.keys(): # for each sequence
-
-        
-
-        string = seq_dict[k]["sequence"]
-        string = string.replace(" ", "").replace("\n", "")
-        substrings = []
-        for i in range(0, len(string), 510):
-            substrings.append(string[i:i+510])
-
-        kmerized_sequences = [split_sequence(seq) for seq in substrings]
-
-        model_inputs = tokenizer(
-            kmerized_sequences,
+    model_inputs = tokenizer(
+            kmerized_sequence,
             add_special_tokens=True,
             padding=True,
             return_tensors="pt"
         )
-
-        with torch.no_grad():
-            embeddings = model(
-                input_ids=model_inputs["input_ids"].to(device),
-                token_type_ids=model_inputs["token_type_ids"].to(device),
-                attention_mask=model_inputs["attention_mask"].to(device),
-            )
-
-        seq_dict[k][ANNOTATION_KEY] = np.array(embeddings.pooler_output).tolist()
+    
+    with torch.no_grad():
+        z = model(
+            input_ids=model_inputs["input_ids"].to(device),
+            token_type_ids=model_inputs["token_type_ids"].to(device),
+            attention_mask=model_inputs["attention_mask"].to(device),
+    )
         
+    z = np.array(z.last_hidden_state).reshape(-1, 768)
 
-    with open(FILE_PATH, "w") as file:
-        json.dump(seq_dict, file, indent=4)
+    if write_to_path != False:
+        np.save(write_to_path, z)
+
+    end = time.time()
+
+    print(f"Time for embedding: {end - start}", file=sys.stderr, flush=True)
+
+    return z
 
 
+def main():
 
-    #torch.save(embeddings, "embeddings.pt")
+    pid = os.getpid()
+    print(f'{pid}, {FASTA_FILE_PATH}', file=sys.stderr, flush=True)
+
+    # check if the output directory exists
+    if not os.path.exists(OUT_DIR):
+        os.makedirs(OUT_DIR)        
+
+    for count, seqrecord in enumerate(SeqIO.parse(FASTA_FILE_PATH, "fasta")):
+
+        seq_id = seqrecord.id
+
+        # check if the file already exists
+        if os.path.exists(os.path.join(OUT_DIR, f"{seq_id}.npy")):
+            print(f"Skipping {seq_id} because already exists", file=sys.stderr, flush=True)
+            continue
+
+        seq_string = str(seqrecord.seq)
+        seq_string = seq_string.replace(" ", "").replace("\n", "")
+
+        if set(seq_string).issubset(set(["A", "C", "G", "T"])):
+            seq_string = str(Seq(seq_string).translate(stop_symbol=""))
+            print("The nucleotides sequence for ", seq_id, " has been translated", file=sys.stderr, flush=True)
+        
+        # split the sequence in chunks such that each chunk has approximately the same length
+        N = int(np.ceil(len(seq_string) / MAX_CHUNK_SIZE)) # number of chunks
+        chunks = [seq_string[(i*len(seq_string))//N:((i+1)*len(seq_string))//N] for i in range(N)] # list of chunks
+
+        lens = [len(chunk) for chunk in chunks]
+
+        if (FAST_MODE):
+            
+            sequence_embedding = []
+            for chunk_index, chunk in enumerate(chunks):
+                print(f"Predicting the embedding {count+1}, chunk {chunk_index+1}/{len(chunks)}", file=sys.stderr, flush=True)
+                z = predict(id=seq_id, chunk_index=chunk_index, query_sequence=chunk, write_to_path=False)    
+                sequence_embedding.append(z)
+
+            # can happen that che subsequences are not of the same length, in this case pad them with the mean value
+            max_len = max([len(z) for z in sequence_embedding]) # z is a np array size (chunk_size x 1280)
+            for i, z in enumerate(sequence_embedding):
+                if len(z) < max_len:
+                    sequence_embedding[i] = np.append(z, [np.mean(z, axis=0)], axis=0) # it is enough to append only one value, since the max difference between chunks is 1
+
+            sequence_embedding = np.array(sequence_embedding)
+            
+            # save the embedding
+            np.save(os.path.join(OUT_DIR, f"{seq_id}.npy"), sequence_embedding)
+        
+        else:
+             
+            # save each chunk in a separate file
+            for chunk_index, chunk in enumerate(chunks):
+                print(f"Predicting the embedding {count+1}, chunk {chunk_index+1}/{len(chunks)}", file=sys.stderr, flush=True)
+                # check if the file already exists
+                if os.path.exists(os.path.join(OUT_DIR, f"{seq_id}_chunk:{chunk_index}.npy")):
+                    print(f"Skipping {seq_id}_chunk:{chunk_index+1} because already exists", file=sys.stderr, flush=True)
+                    continue
+                # run predict in a separate process
+                p = Process(target=predict, args=(seq_id, chunk_index, chunk, os.path.join(OUT_DIR, f"{seq_id}_chunk:{chunk_index}.npy")))
+                p.start()
+                p.join()
+            
+            # load the chunks and recombine them
+            print(f"Recombining the chunks for {seq_id}", file=sys.stderr, flush=True)
+            sequence_embedding = []
+            for chunk_index in range(len(chunks)):
+                z = np.load(os.path.join(OUT_DIR, f"{seq_id}_chunk:{chunk_index}.npy"))
+                sequence_embedding.append(z)
+                # remove the chunk file
+                os.remove(os.path.join(OUT_DIR, f"{seq_id}_chunk:{chunk_index}.npy"))
+
+            # perform the padding as before
+            max_len = max([len(z) for z in sequence_embedding]) # z is a np array size (chunk_size x 1280)
+            for i, z in enumerate(sequence_embedding):
+                if len(z) < max_len:
+                    sequence_embedding[i] = np.append(z, [np.mean(z, axis=0)], axis=0) # it is enough to append only one value, since the max difference between chunks is 1
+
+            sequence_embedding = np.array(sequence_embedding)
+            
+            # save the embedding
+            np.save(os.path.join(OUT_DIR, f"{seq_id}.npy"), sequence_embedding)
 
 
+    print(f'{pid}, DONE', file=sys.stderr, flush=True)
 
 
 if __name__ == "__main__":
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    compute_embeddings(torch.device(device))
-
-
+    main()
